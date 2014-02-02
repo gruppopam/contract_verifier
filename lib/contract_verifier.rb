a745617bc40a8d7cebf95dfc3e3adf20575295da
Priyanka <priyarkala@gmail.com>
2014-02-02 17:03:10 +0530

    #contracts[Priyanka] fixing multiple declaration warning for same url when methods are different

--- lib/contract_verifier.rb
+++ lib/contract_verifier.rb
@@ -7,7 +7,7 @@ module ContractVerifier
     module ContractMatcher
       module ClassMethods
         include Utils
-        
+
         def validate_contract_for_yml(opts)
           stub_root = opts[:stub_root]
           context = opts[:context_path]
@@ -17,10 +17,35 @@ module ContractVerifier
           url = "http://localhost:#{service_port}#{context}/?_wadl"
           full_schema = JSON.parse(Net::HTTP.get(URI(url)))
           service_entries=YAML.load_file(yaml_file)
+          handle_for_resource(context, contract, full_schema, service_entries, yaml_file)
+          handle_removed_end_points(service_entries)
+        rescue Errno::ECONNREFUSED => e
+          puts red("Service is not running in the port #{service_port}".capitalize)
+          raise e
+        end
+
+
+        def validate_stub_for(opts)
+          stub_root = opts[:stub_root]
+          data_root = opts[:data_root]
+          yaml_file = data_root +'/'+ opts[:yml_file]
+          contract = StubSchemaValidator::StubVerifier.new(stub_root, data_root)
+          service_entries=YAML.load_file(yaml_file)
+          service_entries.each do |entry|
+            it("Stub schema verifier for #{opts[:yml_file]} -- #{entry['request']['url']}") do
+              contract.validate entry
+            end
+          end
+        end
+
+
+        private
+
+        def handle_for_resource(context, contract, full_schema, service_entries, yaml_file)
           full_schema['resources'].each do |resource|
             get_url = (context + resource['resource']['path'])
             entry = service_entries.select do |key, hash|
-              key["request"]["url"] == get_url
+              key["request"]["url"] == get_url and key["request"]["method"] == resource["resource"]["method"]
             end
             if entry.length > 1
               it 'Multiple declaration Found' do
@@ -39,28 +64,18 @@ module ContractVerifier
             end
             entry['in_wadl'] = true
           end
-          service_entries.each do |entry|
-            unless entry.has_key?('in_wadl')
-              pending("Service removed #{entry['request']}")
-            end
-          end
-        rescue Errno::ECONNREFUSED => e
-          puts red("Service is not running in the port #{service_port}".capitalize)
-          raise e
         end
 
-        def validate_stub_for(opts)
-          stub_root = opts[:stub_root]
-          data_root = opts[:data_root]
-          yaml_file = data_root +'/'+ opts[:yml_file]
-          contract = StubSchemaValidator::StubVerifier.new(stub_root, data_root)
-          service_entries=YAML.load_file(yaml_file)
+        def handle_removed_end_points(service_entries)
           service_entries.each do |entry|
-            it("Stub schema verifier for #{opts[:yml_file]} -- #{entry['request']['url']}") do
-              contract.validate entry
+            unless entry.has_key?('in_wadl')
+              it "service removed" do
+                pending("Service removed #{entry['request']}")
+              end
             end
           end
         end
+
       end
     end
   end

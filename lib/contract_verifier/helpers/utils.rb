979fe41c5484703de71ec757a3d7e00d0a3d7a19
Raghavan <oneraghavan@gmail.com>
2014-11-17 11:38:42 +0530

    #promo_972 [Raghavan/Prakash] making schema non mandatory for stub - schema verification , making them as pending and response file also made non mandatory

--- lib/contract_verifier/helpers/utils.rb
+++ lib/contract_verifier/helpers/utils.rb
@@ -8,12 +8,13 @@ module ContractVerifier
     end
 
     def data_file_name_for(entry, key)
-      response_file = entry[key]['file']
-      response_body = entry[key]['body']
+      resource = entry[key]
+      response_file = resource['file']
+      response_body = resource['body']
       response = response_file || response_body
       if response.nil?
         if entry['request']['method'] == 'GET'
-          raise "Data Undefined/File not present - #{consumer(entry)}"
+          raise PendingDeclaredInExample.new("No response file defined")
         end
       else
         response_body.nil? ? @data_root+"/#{response_file}" : response_body
@@ -24,7 +25,7 @@ module ContractVerifier
       file_name = entry['response']['schema']
       if file_name.nil?
         unless data_file_name_for(entry, key).nil?
-          raise "Schema Undefined/File not present - #{consumer(entry)}"
+          raise PendingDeclaredInExample.new("Schema Undefined/File not present - #{consumer(entry)}")
         end
       else
         @stub_root+"/#{file_name}"

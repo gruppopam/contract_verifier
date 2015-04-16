8c45d6798665a6757e48edcb5f54408b658790c8
Muralidharan <muralin@thoughtworks.com>
2015-04-16 14:50:09 +0530

    #2792 [Murali/Ginette] - skipping url verification if url contains regex

--- lib/contract_verifier/helpers/stub_schema_validator.rb
+++ lib/contract_verifier/helpers/stub_schema_validator.rb
@@ -26,6 +26,13 @@ module StubSchemaValidator
           consumer_schema = schema_file_name(entry, key)
           should verify_contract(consumer_schema, consumer_data, key) unless (consumer_schema.nil? or consumer_data.nil?)
         end
+
+        return true if consumer_data.nil?
+        return true if entry['request']['url_has_regex']
+
+        actual_file_url = entry['request']['url'].gsub('$','').gsub('^','')
+        actual_file_url.should match_url_in_schema consumer_schema
+
       end
 
     end
